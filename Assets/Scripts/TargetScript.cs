62731ac2c01df2c37b01d032f33635761e14783b
dreruff <gregoryaruffin@gmail.com>
2017-11-12 20:23:32 -0800

    Initial commit

---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetScript : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+
+		//canvas = GetComponent<Canvas> ();
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour> ();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Enable canvas when target is found
+			canvas.enabled = true;
+		}
+		else
+		{
+			// Disable canv when target is lost
+			canvas.enabled = false;
+		}
+	}   
+}
